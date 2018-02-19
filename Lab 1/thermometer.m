9eb11e085c5cc351dfa6df4b3769665409c2295a
Carlos Flores <cflor083@fiu.edu>
2018-02-18 20:28:31 -0500

    Finished code for the GUI & graphs the results as well.

--- Lab 1/thermometer.m
+++ Lab 1/thermometer.m
@@ -7,24 +7,46 @@ tempPin = 'A0';
 speakerPin = 'D6';
 
 temp = 1.0; % Holds the temperature reading.
-tic; % Starts the timer
-elapsedTime = toc; % Used to keep track of the time elapsed.
+iterations = 0; % Count iterations to know when to stop.
+tempArray = []; % Holds all the temp values.
 
-while(elapsedTime <= 10)
+% Read the temperature sensor for 10 seconds (21 iterations with 0.5 pauses).
+% (measure at 0, then up to 10 seconds).
+while(iterations < 21)
     
     % Read output voltage from temp sensor, and convert to temp in C.
     rawVoltage = readVoltage(a, tempPin);
     temp = (rawVoltage - 0.5) * 100;
-    fprintf("TMP35 Vout: %f V | Temp: %f C | Time Elapsed: %fs\n", rawVoltage, temp, elapsedTime);
+    %fprintf("TMP35 Vout: %f V | Temp: %f C | Time Elapsed: %fs\n", rawVoltage, temp, elapsedTime);
     
     % Buzz the speaker if the temperature is above 25C.
     if(temp >= 25.0)
         playTone(a, speakerPin, 1500, 1);
     end
     
+    tempArray = [tempArray, temp];
     pause(0.5);
-    elapsedTime = toc; % Grab current time elapsed
+    iterations = iterations + 1; % Increment iterations count.
 end
+
+% Generate time array.
+time = [];
+for i = 0:+0.5:10
+    time = [time, i];
+end
+lengthTime = length(time); % prepare for smoothing the curve.
+
+% Use splines to interpolate a smoother curve,
+% with 10 times as many points,
+% that goes exactly through the same data points.
+samplingRateIncrease = 10;
+newXSamplePoints = linspace(min(time), max(time), lengthTime * samplingRateIncrease);
+smoothedY = spline(time, tempArray, newXSamplePoints);
+
+% Now flip back
+xSmooth = newXSamplePoints;
+ySmooth = smoothedY;
+plot(xSmooth, ySmooth); % Smoothed curve.
  
 clear % Clear variables.
 
