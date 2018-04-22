c0e4cbfc8ec54ada71d0c405821963d8bc44d100
Carlos Flores <cflor083@fiu.edu>
2018-04-22 19:44:41 -0400

    Added working code for the motor and heating pad, with some offset issues.

--- Lab 4/incubator.m
+++ Lab 4/incubator.m
@@ -3,6 +3,10 @@ a = arduino();
 
 % Pin assignments
 tmpPin = 'A0';
+motorPin = 'D7';
+heatPadPin = 'D6';
+
+writeDigitalPin(a, heatPadPin, 1);
 
 while(1)
    
@@ -11,6 +15,14 @@ while(1)
     tmpInC = (rawVoltage - 0.5) * 100;
     tmpInF = (tmpInC * (9/5)) + 32;
     
+    % Turn on fan when past threshold, or turn on heatPad when too cold.
+    if(tmpInF >= 98)
+        writeDigitalPin(a, heatPadPin, 0);
+        writeDigitalPin(a, motorPin, 1);
+    else
+        writeDigitalPin(a, heatPadPin, 1);
+        writeDigitalPin(a, motorPin, 0);
+    end
     
     fprintf("Temperature: %f deg F\n", tmpInF);
     pause(0.5);
